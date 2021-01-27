80691de6b5da17d013d8f92f48e37cc591b7f47e
Youcef <yelkamel@gmail.com>
2021-01-27 15:41:15 +0100

    use enum direct in the state file

--- lib/enum.dart
+++ lib/enum.dart
@@ -1,24 +1,12 @@
 part of evolum_package;
 
-enum ReminderType { wisdom, night, morning }
-
 enum OnBoardingError { invalidName, none }
 
-enum LoginStep { loading, pwd, name, email }
-
-enum AppState { splash, home, onboarding }
-
 enum LoginError { invalidCode, invalidFormatEmail, emailHasBeenSend, none }
-
-enum ReviewStep { init, askReview, getFeedBack, thanks }
 
 enum PaymentStep { start, form, end }
 
 enum UserStatus { free, challenger, lead, trial, abo, cancel }
-
-enum PlayState { none, playing, paused, buffering, noInternet }
-
-enum ShareType { instastory, fbstory, sms, whatsapp, other }
 
 enum CourseType { adventure, mood }
 
