60fd82afbd61c43ff51029c9f134a46f2d865c0e
Youcef <yelkamel@gmail.com>
2021-01-28 20:17:33 +0100

    update APIPath

--- lib/api_path.dart
+++ lib/api_path.dart
@@ -6,6 +6,8 @@ class APIPath {
   static String user(String uid) => 'user/$uid';
   static String userFav(String uid) => 'user/$uid/info/fav';
   static String userHistory(String uid, String id) => 'user/$uid/history/$id';
+  static String userHistoryId(String uid) => 'user/$uid/info/history';
+
   static String userReco(String uid) => 'user/$uid/info/reco';
 
   static String allCourse() => 'course/';
