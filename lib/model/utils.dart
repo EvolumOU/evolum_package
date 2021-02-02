0e15e318a2e0efb8d82210ed3bedd8fdc5eabf29
Youcef <yelkamel@gmail.com>
2021-02-02 15:14:32 +0100

    nullify dateTime

--- lib/model/utils.dart
+++ lib/model/utils.dart
@@ -1,11 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 DateTime dateTimefromJson(Timestamp date) {
-  if (date == null) return DateTime.now();
+  if (date == null) return null;
   return DateTime.fromMillisecondsSinceEpoch(date.millisecondsSinceEpoch);
 }
 
 Timestamp dateTimetoJson(DateTime date) {
-  if (date == null) return Timestamp.now();
+  if (date == null) return null;
   return Timestamp.fromMicrosecondsSinceEpoch(date.microsecondsSinceEpoch);
 }
