73a2aa94fd8ab0be2c994cac0ba4919cf4244daf
Youcef <youcef@el-kamel.fr>
2021-07-27 16:32:50 +0200

    update list<T> extension

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -17,6 +17,20 @@ extension EvoExtensionList<T> on List<T> {
     if (length == 1) return this[0];
     return this[Random().nextInt(length - 1)];
   }
+
+  List<T> nRandom(int n) {
+    final arrayTmp = this;
+    final List<T> res = [];
+
+    for (var i = 0; i < n; i++) {
+      final evoIndex = arrayTmp.length.fromZeroRandom;
+      final T evoTmp = arrayTmp[evoIndex];
+      arrayTmp.removeAt(evoIndex);
+      res.add(evoTmp);
+    }
+
+    return res;
+  }
 }
 
 extension EvoExtensionEvo on Evo {
