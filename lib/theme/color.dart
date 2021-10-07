624bb634a83b79e113132db5c9a6093e6114d620
Youcef <youcef@el-kamel.fr>
2021-10-07 14:47:44 +0200

    add new colors

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -2,14 +2,14 @@ part of evolum_package;
 
 const Color kevoBlack = Color(0xff241F37); // #241F37
 const Color kevoLightBlack = Color(0xff342F4C); //  #342F4C
-
 const Color kevoGrey = Color(0xffF1F1F2); //  #F1F1F2
-
 const Color kevoWhite = Color(0xffFBF7F1); // #FBF7F1
 const Color kevoFullWhite = Color(0xffFFFFFF); // #FFFFFF
 const Color kevoViolet = Color(0xffC89FE3); //  #C89FE3
 const Color kevoElectric = Color(0xff9EE0FD); //  #9EE0FD
 const Color kevoOrange = Color(0xffFFAC73); //  #FFAC73
+const Color kevoBelge = Color(0xffF3EAE0); // #F3EAE0
+const Color kevoYellow = Color(0xffFCD785); // "#FCD785"
 
 Color oneRandomColor() {
   final List<Color> array = [
