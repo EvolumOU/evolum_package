119c177e27bd58851ab5fd53c59f1b3031b56881
gdelataillade <gautier2406@gmail.com>
2021-10-12 12:47:13 +0200

    Add google storage base url

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,4 +1,5 @@
 part of evolum_package;
 
 const String evoRemoteBaseURL = "https://evolum.s3.eu-west-3.amazonaws.com/";
+const String gsBaseUrl = "gs://evolum-936c6.appspot.com";
 const List<String> playlistTypes = ['seat', 'move', 'repeat', 'listen'];
