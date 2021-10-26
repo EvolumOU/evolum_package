f5a92eae43d8cff928c837520f94f4fe26abc417
Youcef <youcef@el-kamel.fr>
2021-10-26 17:36:38 +0200

    add cloud function utils

---
+++ lib/utils/cloud_function.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:flutter/foundation.dart';
+
+class CloudFunction {
+  static Future<void> getAgoraRitualLiveToken(
+    String channelName,
+    String uid,
+  ) async {
+    HttpsCallable callable = FirebaseFunctions.instance.httpsCallable(
+      'getAgoraRitualLiveToken',
+      options: HttpsCallableOptions(),
+    );
+    final param = {
+      'channelName': channelName,
+      'uid': uid,
+    };
+
+    final results = await callable.call(param);
+
+    debugPrint("===> [CloudFunction] getAgoraToken ${results.data}");
+    return results.data;
+  }
+}
