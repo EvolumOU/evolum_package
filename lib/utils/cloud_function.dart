9a0e522cbc9209d3bd8b45c8d74bc4c4e7ca46a0
Youcef <youcef@el-kamel.fr>
2021-10-27 16:33:58 +0200

    add fixed cloud function utils

--- lib/utils/cloud_function.dart
+++ lib/utils/cloud_function.dart
@@ -1,23 +1,34 @@
-import 'package:cloud_functions/cloud_functions.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 
+const cloudFunctionBastUrl =
+    "https://europe-west1-evolum-936c6.cloudfunctions.net";
+
 class CloudFunction {
-  static Future<String> getAgoraRitualLiveToken(
+  static Future<String?> getAgoraRitualLiveToken(
     String channelName,
     String uid,
   ) async {
-    HttpsCallable callable = FirebaseFunctions.instance.httpsCallable(
-      'getAgoraRitualLiveToken',
-      options: HttpsCallableOptions(),
-    );
-    final param = {
-      'channelName': channelName,
-      'uid': uid,
-    };
+    try {
+      const url = "$cloudFunctionBastUrl/getAgoraRitualLiveToken";
+      debugPrint("===> [CloudFunction] getAgoraRitualLiveToken ");
 
-    final results = await callable.call(param);
-
-    debugPrint("===> [CloudFunction] getAgoraToken ${results.data}");
-    return results.data;
+      final response = await http.post(
+        Uri.parse(url),
+        body: {
+          'channelName': channelName,
+          'uid': uid,
+        },
+      );
+      final jsonDecoded = json.decode(response.body);
+      debugPrint("===> [CloudFunction] response: ${response.body}");
+      if (jsonDecoded["token"] == null) throw (jsonDecoded.toString());
+      return jsonDecoded["token"];
+    } catch (err) {
+      debugPrint("===> [CloudFunction] getAgoraRitualLiveToken error $err");
+      return null;
+    }
   }
 }
