535d5079c3fb729996edc309ebcb39186580e3c5
Youcef <youcef@el-kamel.fr>
2021-11-05 16:33:59 +0100

    fix model utils

--- lib/model/utils.dart
+++ lib/model/utils.dart
@@ -2,12 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 DateTime? dateTimefromJson(Timestamp? date) {
   if (date == null) return null;
-  return DateTime.fromMillisecondsSinceEpoch(date.microsecondsSinceEpoch);
+  return DateTime.fromMillisecondsSinceEpoch(date.millisecondsSinceEpoch);
 }
 
 Timestamp? dateTimetoJson(DateTime? date) {
   if (date == null) return null;
-  return Timestamp.fromMicrosecondsSinceEpoch(date.microsecondsSinceEpoch);
+  return Timestamp.fromMillisecondsSinceEpoch(date.millisecondsSinceEpoch);
 }
 
 Duration? durationfromJson(int? duration) {
