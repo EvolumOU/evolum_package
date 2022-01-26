2cff9a60c6382811d92cd180889a19c13c7aa772
Youcef <youcef@el-kamel.fr>
2022-01-26 11:48:59 +0100

    ⚰️ update evo model

--- lib/model/evo.dart
+++ lib/model/evo.dart
@@ -3,7 +3,6 @@ import 'package:copy_with_extension/copy_with_extension.dart';
 import 'package:evolum_package/model/timing.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-import '../main.dart';
 import 'utils.dart';
 
 part 'evo.g.dart';
@@ -13,15 +12,12 @@ part 'evo.g.dart';
 class Evo {
   // TODO: Make fields final
   String id;
-  String filename;
   String gsUrl;
   String name;
   String type;
   String tag;
-  bool splited;
   @JsonKey(toJson: durationtoJson, fromJson: durationfromJson)
   Duration duration;
-  List<dynamic> points;
   bool loop;
   List<Timing> timings;
   @JsonKey(toJson: dateTimetoJson, fromJson: dateTimefromJson)
@@ -36,14 +32,11 @@ class Evo {
 
   Evo({
     this.id = '',
-    this.filename = '',
     this.gsUrl = '',
     this.name = '',
     this.type = '',
     this.tag = '',
-    this.splited = false,
     this.duration = Duration.zero,
-    this.points = const <dynamic>[],
     this.loop = false,
     this.timings = const <Timing>[],
     required this.createdDate,
@@ -56,10 +49,9 @@ class Evo {
     this.size = 0.0,
   });
 
-  String get remoteUrl => "$evoRemoteBaseURL$filename";
-
-  bool get isVideo => filename.contains(".mp4");
-  bool get isAudio => filename.contains(".mp3");
+  String get filename => gsUrl.split('/').last;
+  bool get isVideo => gsUrl.endsWith(".mp4");
+  bool get isAudio => gsUrl.endsWith(".mp3");
   bool get hasTimming => timings.isNotEmpty;
 
   bool get isVertical => form == "vertical";
@@ -69,18 +61,7 @@ class Evo {
   bool get isNew =>
       DateTime.now().isBefore(createdDate.add(Duration(days: 20)));
 
-  factory Evo.fromJson(Map<String, dynamic> data) => _$EvoFromJson({
-        ...data,
-        "summarize": data["summarize"] ?? '',
-        "intro": data["intro"] ?? '',
-        "gsUrl": data["gsUrl"] ?? '',
-        "filename": data["nafilenameme"] ?? '',
-        "name": data["name"] ?? '',
-        "type": data["type"] ?? '',
-        "tag": data["tag"] ?? '',
-        "form": data["form"] ?? 'focus',
-        "mode": data["mode"] ?? 'vertical',
-      });
+  factory Evo.fromJson(Map<String, dynamic> data) => _$EvoFromJson(data);
 
   Map<String, dynamic> toJson() => _$EvoToJson(this);
 
