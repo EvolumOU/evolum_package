aaadde4cba8dc9f6273bde49abede006bc9096ae
gdelataillade <gautier2406@gmail.com>
2022-06-14 16:33:34 +0200

    ✏️ Update day/night hours naming

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -4,5 +4,5 @@ const String evoRemoteBaseURL = "https://evolum.s3.eu-west-3.amazonaws.com/";
 const String gsBaseUrl = "gs://evolum-936c6.appspot.com";
 const List<String> playlistTypes = ['seat', 'move', 'repeat', 'listen'];
 
-const ritualDayHours = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const ritualNightHours = [0, 1, 2, 3, 4, 16, 17, 18, 19, 20, 21, 22, 23];
+const dayHours = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const nightHours = [0, 1, 2, 3, 4, 16, 17, 18, 19, 20, 21, 22, 23];
