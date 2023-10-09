86d5bc1a3d32d83a1a695ccf0bf119f3f8369b03
Youcef <yelkamel@gmail.com>
2023-10-09 17:15:44 +0200

    🚚 fix import

--- lib/model/ambiance.dart
+++ lib/model/ambiance.dart
@@ -1,5 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
-import 'package:evolum_package/model/utils.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'ambiance.g.dart';
@@ -7,7 +7,7 @@ part 'ambiance.g.dart';
 const firestoreSerializable = JsonSerializable(
   converters: firestoreJsonConverters,
   explicitToJson: true,
-  // createFieldMap: true,
+  createFieldMap: true,
 );
 
 @firestoreSerializable
